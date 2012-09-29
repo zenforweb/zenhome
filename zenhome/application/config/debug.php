e7786740cd36a1270e6374f114932dadc8eb259a
Chris Robak <me@chrisrobak.com>
2012-09-29 17:49:43 -0600

    forced commit to absorb upstream

--- zenhome/application/config/debug.php
+++ zenhome/application/config/debug.php
@@ -1,6 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
 	// DEVELOPMENT CONFIG
 
 	define( 'DEVELOPMENT', TRUE ) ;
-	define( 'DEVELOPMENT_PATH', 'http://10.1.10.52/~alix/zenhome' );
+	define( 'DEVELOPMENT_PATH', 'http://10.1.10.52/~chris/zenhome' );
