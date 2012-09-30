b780c4cb9b6d0fccee321a2c404c24f00559d88c
Alix Fullerton <alix@politeauthority.com>
2012-09-29 21:05:00 -0600

    Fixes for outside controller, updates for Apps, and navigation

--- zenhome/application/controllers/apps/apps.php
+++ zenhome/application/controllers/apps/apps.php
@@ -1,16 +1,16 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Apps extends CI_Controller {
+class Apps extends MY_Controller {
 
-				/**
-				 * Index Page for this controller.
-				 *
-				 * Maps to the following URL
-				 *              http://example.com/plugins
-				 *      - or -
-				 *              http://example.com/index.php/plugins/index
-				 *
-				 */
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 *              http://example.com/plugins
+	 *      - or -
+	 *              http://example.com/index.php/plugins/index
+	 *
+	 */
 
 	public function __construct(){
 		parent::__construct();
@@ -21,28 +21,7 @@ class Apps extends CI_Controller {
 		$this->load->model('AccountModel');
 		$this->user = $this->AccountModel->userInfo( $_SESSION['user_id'] );			
 	}
-
-	private function view( $view, $data = Null ){
-		if( isset( $_SESSION['guest'] ) ){
-			$this->load->view('private/header_guest');
-		} else {
-			$menu = array( 
-				array( 'Dashboard', 'dashboard'), 
-				array('Admin', 'admin'), 
-				array( 'Profile', 'profile' ),
-				array( 'Devices', 'devices' ),
-				array( 'Apps', 'apps' ),
-				array( 'Logout', 'outside/logout' ),
-			);
-			$header = array( 
-				'menu' => $menu,
-				'user' => $this->user,
-			);
-			$this->load->view('private/header_private', $header );
-		}
-		$this->load->view( $view, $data );
-	}
-
+	
 	public function index(){
 		 // show all devices, and management
 			$this->view('private/apps');
--- zenhome/application/core/MY_Controller.php
+++ zenhome/application/core/MY_Controller.php
@@ -5,37 +5,47 @@
  */
 class MY_Controller extends CI_Controller{
 
-    function __construct()
-    {
-        parent::__construct();
-    }
+  function __construct(){
+      parent::__construct();
+  }
 
-    private function load_user(){
-    	$this->load->model('AccountModel');
-			$this->user = $this->AccountModel->userInfo( $_SESSION['user_id'] );        
-    }
+  private function load_user(){
+  	$this->load->model('AccountModel');
+		$this->user = $this->AccountModel->userInfo( $_SESSION['user_id'] );        
+  }
 
-    public function view( $view, $data = Null ){
-    	$this->load_user();
-			if( isset( $_SESSION['guest'] ) ){
-				$this->load->view('private/header_guest');
-			} else {
-				$menu = array( 
-					array( 'Dashboard', 'dashboard'), 
-					array('Admin', 'admin'), 
-					array( 'Profile', 'profile' ),
-					array( 'Devices', 'devices' ),
-					array( 'Apps', 'apps' ),
-					array( 'Logout', 'outside/logout' ),
-				);
-				$header = array( 
-					'menu' => $menu,
-					'user' => $this->user,
-				);
-				$this->load->view('private/header_private', $header );
+	private function main_menu(){
+		$menu = array( 
+			array( 'Dashboard', 'dashboard',  		0 ), 
+			array( 'Admin', 		'admin',					0 ), 
+			array( 'Profile', 	'profile', 				0 ),
+			array( 'Devices', 	'devices', 				0 ),
+			array( 'Apps', 			'apps', 					0 ),
+			array( 'Logout', 		'outside/logout', 0 ),
+		);
+		foreach ($menu as $key => $item) {
+			if ( strstr( $_SERVER['REQUEST_URI'], $item[1] ) ){
+				$menu[$key][2] = 1;
 			}
+		}
+		return $menu;
+	}
+
+  public function view( $view, $data = Null ){  	
+  	$this->load_user();
+		if( isset( $_SESSION['guest'] ) ){
+			$this->load->view('private/header_guest');
+		} else {
+			$header = array( 
+				'menu' => $this->main_menu(),
+				'user' => $this->user,
+			);
+			$this->load->view('private/header_private', $header );
+		}
 		$this->load->view( $view, $data );
 	}
+
+
 }
 // END Controller class
 
